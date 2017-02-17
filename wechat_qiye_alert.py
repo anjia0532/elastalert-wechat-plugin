e97d0f3ea2e9fc41c6563e96bf434d24a24c0e27
anjia0532 <anjia0532@gmail.com>
2017-02-17 17:57:36 +0800

    修改中文报错问题以及长度限制

--- wechat_qiye_alert.py
+++ wechat_qiye_alert.py
@@ -1,11 +1,15 @@
-# encoding: utf-8
+#! /usr/bin/env python 
+# -*- coding: utf-8 -*-
+
+#from __future__ import unicode_literals 
 import urllib,urllib2
 import json
 import sys
 import datetime
 from elastalert.alerts import Alerter, BasicMatchString
-
+from requests.exceptions import RequestException
 from elastalert.util import elastalert_logger
+import requests
 
 '''
 ##########################################################
@@ -28,6 +32,7 @@ class WeChatAlerter(Alerter):
         self.corp_id = self.rule.get('corp_id', '') 	#企业号id
         self.secret = self.rule.get('secret', '') 		#secret
         self.agent_id = self.rule.get('agent_id', '')	#应用id
+
         self.party_id = self.rule.get('party_id') 		#部门id
         self.user_d = self.rule.get('user_d', '') 		#用户id，多人用 | 分割，全部用 @all
         self.tag_id = self.rule.get('tag_id', '') 		#标签id
@@ -45,7 +50,7 @@ class WeChatAlerter(Alerter):
         # http://qydev.weixin.qq.com/wiki/index.php?title=AccessToken
         self.get_token()
         
-        print self.access_token
+        #print self.access_token
         #print self.expires_in
 
         self.senddata(body)
@@ -77,26 +82,40 @@ class WeChatAlerter(Alerter):
         return self.access_token
 
     def senddata(self, content):
+        
+        if len(content) > 2000:
+            content = content[:1997] + "..."
+        
         send_url = 'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=' + self.access_token
-        send_values = {
-            "totag":self.tag_id,
-            "touser":self.user_d,
-            "toparty": self.party_id,
-            "msgtype":"text",
-            "agentid":self.agent_id,
+
+
+        # post to pagerduty
+        headers = {'content-type': 'application/json'}
+        payload = {
+            'totag': self.tag_id,
+            'msgtype': "text",
+            "agentid": self.agent_id,
             "text":{
-                "content":content
+                "content": content.encode('UTF-8')
                },
             "safe":"0"
-            }
+        }
 
-        send_data = json.dumps(send_values, ensure_ascii=False)
+        print len(content.encode('UTF-8'))
 
-        send_request = urllib2.Request(send_url, send_data)
+        # set https proxy, if it was provided
+        # proxies = {'https': self.pagerduty_proxy} if self.pagerduty_proxy else None
+        try:
+            response = requests.post(send_url, data=json.dumps(payload, ensure_ascii=False), headers=headers)
+            # response.raise_for_status()
+        except RequestException as e:
+            raise EAException("Error posting to pagerduty: %s" % e)
+        elastalert_logger.info("Trigger sent to PagerDuty")
 
-        response = json.loads(urllib2.urlopen(send_request).read())
-
-        print str(response)
+        print "-----"
+        print response.text
+        print "-----"
+       
 
     def get_info(self):
         return {'type': 'WeChatAlerter'}
